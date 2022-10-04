e5cb8a3e65567453a9c911820b481341590007f3
Quyet Nguyen <quyetnn2051987@gmail.com>
2022-10-04 15:00:51 +0700

    update code

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -3,16 +3,38 @@ import 'source-map-support/register'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-import { getUserId } from '../utils';
-import { createTodo } from '../../businessLogic/todos'
+import { createTodoItem } from '../../helpers/todos'
+import { createLogger } from '../../utils/logger'
 
+const logger = createLogger('todos');
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const newTodo: CreateTodoRequest = JSON.parse(event.body)
-    // TODO: Implement creating a new TODO item
 
-    return undefined
-)
+    if(!newTodo.name)
+    {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          error: 'ERROR: The name is empty.'
+        })
+      };
+    }
+
+    const todo = await createTodoItem(event, newTodo);
+    logger.info("Todo item has been created");
+    return {
+      statusCode: 201,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+  
+      body: JSON.stringify({
+        item: todo
+      })
+    };
+  })
 
 handler.use(
   cors({
